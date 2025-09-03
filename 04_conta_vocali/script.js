417d8ec46b2af3547c4fd89d97d5161d7f478cd2
Teo <seminara.teodoro93@gmail.com>
2025-09-03 16:47:20 +0200

    esercizio 4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,27 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function cercaVocali(parola) {
+    let letteraParola = 0;
+    let vocale = ["a", "e", "i", "o", "u"];
+    for (i = 0; i < parola.length; i++) {
+
+        for (j = 0; j < vocale.length; j++) {
+
+            if (parola[i] === vocale[j]) {
+                letteraParola++;
+            }
+        }
+    }
+
+    return letteraParola
+
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(cercaVocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
